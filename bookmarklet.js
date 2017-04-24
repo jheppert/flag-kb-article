9c2478f0155d95029d28fd2c741dc13682ebaf0b
Jeff Heppert <jeffheppert@Jeffs-MacBook-Pro-2.local>
2017-04-24 11:04:20 -0700

    forgot to make time a query string

--- bookmarklet.js
+++ bookmarklet.js
@@ -7,7 +7,7 @@ javascript: (function() {
             e || this.readyState && 'loaded' != this.readyState && 'complete' != this.readyState || (e = !0, b())
         }, d.appendChild(c)
     }
-    loadScript('//jheppert.github.io/flag-kb-article/remote-script.js' + new Date().getMilliseconds(), function() {
+    loadScript('//jheppert.github.io/flag-kb-article/remote-script.js?' + new Date().getMilliseconds(), function() {
         flagKBArticle.init()
     });
 
